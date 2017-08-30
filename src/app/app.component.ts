e1c509fed5f3a8f0bb492dcd03d5e5859291ea79
geekysuman <suman.sarkar@imaginea.com>
2017-08-30 19:05:02 +0530

    Added realtime graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,98 @@ import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  items: FirebaseListObservable<any[]>;
-  constructor(private db: AngularFireDatabase) {
-    this.items = db.list('/items');
-  }
-  
-  addNewItem(nameBox): void {
-    const item = nameBox.value
-    this.items.push({
-      name: item
-    })
-    nameBox.value = ""
-  }
-  deleteItem(item: any): void {
-    this.db.object('/items/' + item.$key).remove();
-  }
+    title = 'app';
+    items: FirebaseListObservable<any[]>;
+    chart: any;
+    options: Object;
+    addValue: any;
+    chartData: Array<Object>;
+    itemsRef;
+    constructor(private db: AngularFireDatabase) {
+        this.items = db.list('/items');
+        this.showAllItems();
+        this.itemsRef = db.database.ref('/items');
+        this.itemsRef.on('child_added', (data) => {
+            this.addChartPoint(data);
+        });
+        this.itemsRef.on('child_changed', (data) => {
+            this.updateChartData(data);
+        });
+        this.itemsRef.on('child_removed', (data) => {
+            this.deleteChartPoint(data);
+        });
+        this.options = {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Most favourite browser'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: 'black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: []
+            }]
+        };
+    }
+
+    addNewItem(nameBox): void {
+        const item = nameBox.value
+        this.items.push({
+            name: item,
+            value: 0
+        })
+        nameBox.value = ""
+    }
+    poleItem(item: any): void {
+        this.db.object('/items/' + item.$key)
+        .update({ value: (item.value + 1)});
+    }
+    deleteItem(item: any): void {
+        this.db.object('/items/' + item.$key).remove();
+    }
+    saveInstance(chartInstance) {
+        this.chart = chartInstance;
+    }
+    showAllItems() {
+        console.log("items : ", this.items);
+    }
+    addChartPoint(data){
+        this.chart.series[0].addPoint({
+            name: data.val().name,
+            y: data.val().value
+        })
+    }
+    updateChartData(data){
+        const index = this.chart.series[0].data.findIndex(x => x.name==data.val().name);
+        this.chart.series[0].data[index].update({ y: data.val().value });
+    }
+    deleteChartPoint(data){
+        const index = this.chart.series[0].data.findIndex(x => x.name==data.val().name);
+        this.chart.series[0].removePoint(index);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MdCardModule} from '@angular/material';
+import { MdCardModule } from '@angular/material';
+import { ChartModule } from 'angular2-highcharts';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
+import { SimpleChartExample } from './chart/simple-chart.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleChartExample
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    MdCardModule
+    MdCardModule,
+    ChartModule.forRoot(require('highcharts'))
   ],
   providers: [],
   bootstrap: [AppComponent]
